53b359801d4d3dd3da79a58572c2d02265156483
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2024-01-23 11:40:51 +0530

    minor bug fixes

---
+++ frontend/index.js
@@ -0,0 +1,49 @@
+//fetching data from backend
+async function fetchData() {
+    document.querySelector("tbody").innerHTML = "";
+  fetch("https://internship-backend-service.onrender.com/api/v1/get")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      // Creating table row depending on the data, limited to 8 rows
+      data.data.slice(0, 8).forEach((ticker, i) => {
+        const markup = `<tr class="table_row">
+            <td>${i + 1}</td>
+            <td>${ticker.name}</td>
+            <td>${ticker.last}</td>
+            <td>${ticker.buy} / ${ticker.sell}</td>
+            <td>${ticker.volume}</td>
+            <td>${ticker.base_unit}</td>
+        </tr>`;
+        // Inserting markup on tbody
+        document.querySelector("tbody").insertAdjacentHTML("beforeend", markup);
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
+async function updateData() {
+  await fetch("https://internship-backend-service.onrender.com/api/v1/fetch");
+  await fetchData();
+}
+
+fetchData();
+
+const toggle = document.querySelector(".toogle_btn");
+//toggle button functionality for light/dark mode
+toggle.onclick = function () {
+  toggle.classList.toggle("active");
+  document.body.classList.toggle("light_theme");
+};
+
+let timer = document.querySelector(".timer");
+let timeleft = 60;
+let downloadTimer = setInterval(function () {
+  if (timeleft <= 0) {
+    timeleft = 60;
+    updateData();
+  } else {
+    timer.innerHTML = timeleft;
+  }
+  timeleft -= 1;
+}, 1000);
